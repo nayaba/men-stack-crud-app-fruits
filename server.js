d73d3584aa22163d04fa12f6417525177edcc519
Nabila Ayaba <nabilaayaba@gmail.com>
2024-07-21 14:48:54 +0300

    Build create fruit functionality in a POST route

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
-require('dotenv').config()
+require("dotenv").config()
 const express = require("express")
-const morgan = require('morgan')
+const morgan = require("morgan")
 
 const app = express()
 
 // MONGOOSE CONNECTION
-require('./config/database')
+require("./config/database")
+
+const Fruit = require("./models/fruit")
+app.use(express.urlencoded({ extended: false }))
 
 // MIDDLEWARE
-app.use(morgan('dev'))
+app.use(morgan("dev"))
 
 // ROUTES
-app.get('/', async (req, res) => {
-    res.render('index.ejs')
+app.get("/", async (req, res) => {
+  res.render("index.ejs")
 })
 
-app.get('/fruits/new', (req, res) => {
-    res.render('fruits/new.ejs')
+app.get("/fruits/new", (req, res) => {
+  res.render("fruits/new.ejs")
+})
+
+app.post("/fruits", async (req, res) => {
+  req.body.isReadyToEat === "on"
+    ? (req.body.isReadyToEat = true)
+    : (req.body.isReadyToEat = false)
+  console.log(req.body)
+  await Fruit.create(req.body)
+  res.redirect("/fruits/new")
 })
 
 app.listen(3000, () => {
