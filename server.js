c0876177ac687a620359b7fe1858d4663ad82c7d
Nabila Ayaba <nabilaayaba@gmail.com>
2024-07-21 16:32:02 +0300

    Create show route and view

--- server.js
+++ server.js
@@ -1,43 +1,50 @@
-require("dotenv").config()
-const express = require("express")
-const morgan = require("morgan")
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
 // MONGOOSE CONNECTION
-require("./config/database")
+require('./config/database')
 
-const Fruit = require("./models/fruit")
+const Fruit = require('./models/fruit')
 app.use(express.urlencoded({ extended: false }))
 
 // MIDDLEWARE
-app.use(morgan("dev"))
+app.use(morgan('dev'))
 
 // ROUTES
-app.get("/", async (req, res) => {
-  res.render("index.ejs")
+app.get('/', async (req, res) => {
+  res.render('index.ejs')
 })
 
 app.get('/fruits', async (req, res) => {
-    const allFruit = await Fruit.find({})
-    res.render('fruits/index.ejs', {
-        fruits: allFruit
-    })
+  const allFruit = await Fruit.find({})
+  res.render('fruits/index.ejs', {
+    fruits: allFruit,
+  })
 })
 
-app.get("/fruits/new", (req, res) => {
-  res.render("fruits/new.ejs")
+app.get('/fruits/new', (req, res) => {
+  res.render('fruits/new.ejs')
 })
 
-app.post("/fruits", async (req, res) => {
-  req.body.isReadyToEat === "on"
+app.get('/fruits/:fruitID', async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitID)
+  res.render('fruits/show.ejs', {
+    fruit: foundFruit
+  })
+})
+
+app.post('/fruits', async (req, res) => {
+  req.body.isReadyToEat === 'on'
     ? (req.body.isReadyToEat = true)
     : (req.body.isReadyToEat = false)
   console.log(req.body)
   await Fruit.create(req.body)
-  res.redirect("/fruits")
+  res.redirect('/fruits')
 })
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000")
+  console.log('Listening on port 3000')
 })
