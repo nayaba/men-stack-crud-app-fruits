997300f243890a6af6eb5a2efaecf1bfd5d0c277
Nabila Ayaba <nabilaayaba@gmail.com>
2024-07-21 12:32:45 +0300

    Connect to mongodb fruits 🍒

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+require('dotenv').config()
 const express = require("express")
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 const app = express()
+
+// MONGOOSE CONNECTION
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name} 🍒`)
+})
 
 // MIDDLEWARE
 app.use(morgan('dev'))
